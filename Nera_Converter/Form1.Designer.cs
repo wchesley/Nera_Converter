8dbd67e71901cb60754a052235c5e8578bc17a1c
Kiyanzalegend <chesley.walker@gmail.com>
2017-11-09 23:24:53 -0600

    init commit

--- Nera_Converter/Form1.Designer.cs
+++ Nera_Converter/Form1.Designer.cs
@@ -28,12 +28,73 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(105, 93);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(166, 64);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Choose File";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(105, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File: ";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtBox1.Enabled = false;
+            this.txtBox1.Location = new System.Drawing.Point(192, 37);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(481, 31);
+            this.txtBox1.TabIndex = 2;
+            // 
+            // btn2
+            // 
+            this.btn2.Enabled = false;
+            this.btn2.Location = new System.Drawing.Point(105, 194);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(166, 64);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "Load File";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 626);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Button btn2;
     }
 }
 
--- Nera_Converter/Form1.cs
+++ Nera_Converter/Form1.cs
@@ -7,6 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ExcelDataReader;
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
 
 namespace Nera_Converter
 {
@@ -15,6 +20,100 @@ namespace Nera_Converter
         public Form1()
         {
             InitializeComponent();
+
+        }
+        
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog excelFile = new OpenFileDialog();
+            excelFile.ShowDialog();
+            excelFile.Title = "Choose Excel File: ";
+            excelFile.InitialDirectory = @"C:\";
+            excelFile.DefaultExt = "xlsx"+"xls";
+            excelFile.CheckFileExists = true;
+            excelFile.CheckPathExists = true; 
+            if(excelFile.ShowDialog() == DialogResult.OK)
+            {
+                txtBox1.Text = excelFile.FileName;
+                btn2.Enabled = true;    
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+
+                var NenaFile = new Excel.Application();
+                NenaFile.DisplayAlerts = false;
+                NenaFile.Visible = false;
+                var NenaWorkbook = (Excel.Workbook)(NenaFile.Workbooks.Open(txtBox1.Text, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value));
+                Excel.Worksheet NenaSheet = NenaWorkbook.Sheets[1];
+                Excel.Range rawNenaColumn_N = NenaSheet.Range["N2","N11562"].Value2;
+                Excel.Range formattedColumn_StreetDir = NenaSheet.Range["J2", "J11562"].Value2;
+                string[] Column_N_Array = rawNenaColumn_N.Value2.ToString();
+                string[] Column_J_Array = formattedColumn_StreetDir.Value2.ToString();
+                for(int i = 0; i <= Column_N_Array.Length; i++)
+                {
+                    switch (Column_N_Array[i])
+                    {
+                        case "S":
+                            Column_J_Array[i] = Column_N_Array[i];
+                            break;
+                        case "N":
+                            Column_J_Array[i] = Column_N_Array[i];
+                            break;
+                        case "E":
+                            Column_J_Array[i] = Column_N_Array[i];
+                            break;
+                        case "W":
+                            Column_J_Array[i] = Column_N_Array[i];
+                            break;
+
+                    }
+
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+
+            /*ExcelData Reader can't write to excel file 
+            var file = new FileInfo(txtBox1.Text);
+            using (var stream = new FileStream(txtBox1.Text, FileMode.Open, FileAccess.Read))
+            {
+                IExcelDataReader excelReader;
+                if (file.Extension == ".xls")
+                {
+                    excelReader = ExcelReaderFactory.CreateBinaryReader(stream);
+                }
+                else if (file.Extension == ".xlsx")
+                {
+                    excelReader = ExcelReaderFactory.CreateReader(stream);
+                }
+                else throw new Exception("Invalid File");
+                
+                while(excelReader.Read())
+                {
+                    var direction = excelReader.GetValue(13);
+                    switch (direction.ToString())
+                    {
+                        case "S"
+                            excelReader.
+                            break;
+                        case "N":
+                            break;
+                        case "E":
+                            break;
+                        case "W":
+                            break;
+                    }
+                }
+            }
+            */
         }
     }
 }
