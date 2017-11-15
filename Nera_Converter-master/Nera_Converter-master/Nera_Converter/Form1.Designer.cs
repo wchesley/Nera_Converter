3fd0f2b9a08bdcc87a70871a675185c993a00ee2
Walker Chesley <w0448287@amarillocollege.com>
2017-11-14 18:02:11 -0600

    added files

---
+++ Nera_Converter-master/Nera_Converter-master/Nera_Converter/Form1.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Nera_Converter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBox1 = new System.Windows.Forms.TextBox();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.lblCounter = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(52, 48);
+            this.btn1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(83, 33);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "Choose File";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "File: ";
+            // 
+            // txtBox1
+            // 
+            this.txtBox1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.txtBox1.Enabled = false;
+            this.txtBox1.Location = new System.Drawing.Point(96, 19);
+            this.txtBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtBox1.Name = "txtBox1";
+            this.txtBox1.Size = new System.Drawing.Size(242, 20);
+            this.txtBox1.TabIndex = 2;
+            // 
+            // btn2
+            // 
+            this.btn2.Enabled = false;
+            this.btn2.Location = new System.Drawing.Point(52, 101);
+            this.btn2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(83, 33);
+            this.btn2.TabIndex = 3;
+            this.btn2.Text = "Load File";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // lblCounter
+            // 
+            this.lblCounter.AutoSize = true;
+            this.lblCounter.Location = new System.Drawing.Point(154, 101);
+            this.lblCounter.Name = "lblCounter";
+            this.lblCounter.Size = new System.Drawing.Size(13, 13);
+            this.lblCounter.TabIndex = 4;
+            this.lblCounter.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(362, 326);
+            this.Controls.Add(this.lblCounter);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.txtBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn1);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtBox1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Label lblCounter;
+    }
+}
+
---
+++ Nera_Converter-master/Nera_Converter-master/Nera_Converter/Form1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using ExcelDataReader; will remove when interop is working. 
+using System.IO;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.Reflection;
+
+
+namespace Nera_Converter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        
+
+        private void btn1_Click(object sender, EventArgs e)
+        {
+            //actual loading of sheet. 
+            OpenFileDialog excelFile = new OpenFileDialog();
+            excelFile.ShowDialog();
+            excelFile.Title = "Choose Excel File: ";
+            excelFile.InitialDirectory = @"C:\";
+            excelFile.DefaultExt = "xlsx"+"xls";
+            excelFile.CheckFileExists = true;
+            excelFile.CheckPathExists = true; 
+            if(excelFile.ShowDialog() == DialogResult.OK)
+            {
+                txtBox1.Text = excelFile.FileName;
+                
+                btn2.Enabled = true;    
+            }
+        }
+
+        private void btn2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //load excel sheet
+                var NenaFile = new Excel.Application();
+                NenaFile.DisplayAlerts = false;
+                NenaFile.Visible = false;
+                var NenaWorkbook = NenaFile.Workbooks.Open(txtBox1.Text, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+                Excel.Worksheet NenaSheet = NenaWorkbook.Sheets[1];
+
+
+                //current limit on dv list is 11562
+                Excel.Range rawNenaColumn_N = NenaSheet.get_Range("N2", "N11562"); 
+                Excel.Range rawNenaColumn_L = NenaSheet.get_Range("J2", "J11562");
+                Excel.Range rawNenaColumn_O = NenaSheet.get_Range("O2", "O11562");
+                Excel.Range rawNenaColumn_P = NenaSheet.get_Range("P2", "P11562");
+                Excel.Range rawNenaColumn_Q = NenaSheet.get_Range("Q2", "Q11562");
+                Excel.Range rawNenaColumn_R = NenaSheet.get_Range("R2", "R11562");
+                Excel.Range rawNenaColumn_S = NenaSheet.get_Range("S2", "S11562");
+                
+
+
+                //populate arrays from excel sheet, popualte array with cell value(s). 
+                Array Column_N = (Array)rawNenaColumn_N.Cells.Value;  
+                Array Column_L = (Array)rawNenaColumn_L.Cells.Value;
+                Array Column_O = (Array)rawNenaColumn_O.Cells.Value;
+                Array Column_P = (Array)rawNenaColumn_P.Cells.Value;
+                Array Column_Q = (Array)rawNenaColumn_Q.Cells.Value;
+                Array Column_R = (Array)rawNenaColumn_R.Cells.Value;
+                Array Column_S = (Array)rawNenaColumn_S.Cells.Value;
+                string addr = string.Empty;
+                string space = " ";
+
+                //iterate over columns, move data over and aggregate streets seperated by name. 
+                for (int r = 1; r <= Column_N.Length; r++)
+                {
+                    lblCounter.Text = r.ToString();
+                    if(Column_N.GetValue(r,1) == null)
+                    {
+                        Column_N.SetValue(" ", r, 1);
+                    }
+                    string N_Direction = Column_N.GetValue(r, 1).ToString();
+                    //string L_Driection = Column_L.GetValue(r,1).ToString();
+                    if (Column_O.GetValue(r, 1) == null)  
+                    {
+                        Column_O.SetValue(" ", r, 1); 
+                    }
+                    string col_O = Column_O.GetValue(r,1).ToString();
+                    if (Column_P.GetValue(r, 1) == null)
+                    {
+                        Column_P.SetValue(" ", r, 1);
+                    }
+                    string col_P = Column_P.GetValue(r,1).ToString();
+                    if (Column_Q.GetValue(r, 1) == null)
+                    {
+                        Column_Q.SetValue(" ", r, 1);
+                    }
+                    string col_Q = Column_Q.GetValue(r,1).ToString();
+                    if (Column_R.GetValue(r, 1) == null)
+                    {
+                        Column_R.SetValue(" ", r, 1);
+                    }
+                    string col_R = Column_R.GetValue(r,1).ToString();
+                    if (Column_S.GetValue(r, 1) == null)
+                    {
+                        Column_S.SetValue(" ", r, 1);
+                    }
+                    string col_S = Column_S.GetValue(r,1).ToString();
+                    switch (N_Direction)
+                    {
+                        case "S":
+                        case "N":
+                        case "E":
+                        case "W":
+                        case "SW":
+                        case "SE":
+                        case "NE":
+                        case "NW":
+                            {
+                                
+                                NenaSheet.Cells[RowIndex: $"J{r}"] = N_Direction.ToString();
+                            }
+                            break;
+                        default:
+                            {
+                                addr += N_Direction + space + col_O + space;
+                                addr += col_P + space + col_Q + space + col_R + space;
+                                addr += col_S;
+                                NenaSheet.Cells[RowIndex: $"J{r}"] = addr;
+                            }
+                            break;
+                    }
+                    //Excel.Range rawNenaColumn_K = NenaSheet.get_Range("K2", "K11562");
+                    //Excel.Range TestColumnJ = NenaSheet.get_Range("J2","J11562");
+                    //MessageBox.Show("Breakpoint!");
+                    
+                }
+                //NenaWorkbook.Save();
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.ToString());
+            }
+            MessageBox.Show("Done.");
+        }
+    }
+}
